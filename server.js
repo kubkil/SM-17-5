731d45d8ad1e7d72cfd396837a35799157b55b99
Jakub Kilar <kuba.kilar@gmail.com>
2018-05-12 14:35:46 +0200

    refactor

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 const express = require('express');
 const app = express();
 
-// różnica przy takim rozwiązaniu?
-// const store = (req, res, next) => {
-//   console.log('Jestem pośrednikiem przy żądaniu do /store');
-//   next();
-// }
-
-// const bye = (req, res, next) => {
-//   console.log('Bye');
-//   res.end();
-// }
-
 app.use('/store', (req, res, next) => {
   console.log('Jestem pośrednikiem przy żądaniu do /store');
   next();
@@ -20,12 +9,6 @@ app.use('/store', (req, res, next) => {
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
-
-// app.use(store);
-
-// app.get('/store', (req, res) => {
-//   console.log('To jest sklep');
-// }, store);
 
 app.get('/store', (req, res) => {
   console.log('To jest sklep');
